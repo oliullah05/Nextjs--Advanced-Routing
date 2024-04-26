59bbcbf1f6999fdb56bc226a420c17e4a24f793c
oliullah05 <mdolihasan@gmail.com>
2024-04-26 15:57:35 +0600

    buggy parallel-routes show done using (create-route-group) folder

---
+++ app/parallel-dashboard/@quiz/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1 className="text-yellow-500">Loading ......... Quiz</h1>
+        </div>
+    );
+};
+
+export default Loading;
