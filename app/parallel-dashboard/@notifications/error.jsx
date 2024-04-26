9c1f9988c5acb1bff04f8f4103728893124e30aa
oliullah05 <mdolihasan@gmail.com>
2024-04-26 16:08:14 +0600

    catch error and conditional perallel-route show

---
+++ app/parallel-dashboard/@notifications/error.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+const error = () => {
+    return (
+        <div className="h-full w-full m-auto">
+            <h1 className="text-red-500"> ann Error capture in NOTIFICITION</h1>
+        </div>
+    );
+};
+
+export default error;
--- app/parallel-dashboard/layout.js
+++ app/parallel-dashboard/layout.js
@@ -1,9 +1,10 @@
 export default function DashboardLayout({ children, assignments, notifications, quiz }) {
+    const isLoggedIn = true;
     return (
         <>{children}
             <div className="grid gap-6 grid-cols-2 grid-rows-2 p-8">
                 {assignments}
-                {notifications}
+                {isLoggedIn && notifications}
                 {quiz}
             </div>
         </>
