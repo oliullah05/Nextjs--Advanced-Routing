42a8ec1320d5f061e9c13fb7199445ae3a617b95
oliullah05 <mdolihasan@gmail.com>
2024-04-27 19:14:33 +0600

    primary route change done based on prefer language

--- middleware.js
+++ middleware.js
@@ -3,31 +3,37 @@ import { match } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
 
+let defaultLocale = "en";
+let locales = ["en", "bn"];
 
-let defaultLocale = "en"
-let locales =["en","bn"]
+function getLocale(request) {
+    const acceptedLanguage =
+        request.headers.get("accept-language") ?? undefined;
+    const headers = { "accept-language": acceptedLanguage };
+    const languages = new Negotiator({ headers }).languages();
 
-const getLocale = (request)=>{
-const accpectedLanguage = request.headers.get("accept-language") ?? undefined;
-let headers = { 'accept-language': accpectedLanguage }
-const languages = new Negotiator({ headers }).languages();
-match(languages, locales, defaultLocale)
+    return match(languages, locales, defaultLocale); // en or bn
 }
 
 
-export function middleware (request){
+export function middleware(request) {
     // get the pathname from request url
+    const pathname = request.nextUrl.pathname;
 
-    const pathname = request.nextUrl.pathname
+    const pathNameIsMissingLocale = locales.every(
+        (locale) =>
+            !pathname.startsWith(`/${locale}`) &&
+            !pathname.startsWith(`/${locale}/`)
+    );
 
-    const pathnameIsMissingLocale = locales.every(locale=>!pathname.startWith(`/${locale}`) && !pathname.startWith(`/${locale}/`));
-
-    if(pathnameIsMissingLocale){
-        // detect user's preference & redirect with a locale with a path
+    if (pathNameIsMissingLocale) {
+        // detect user's preference & redirect with a locale with a path eg: /en/about
         const locale = getLocale(request);
 
-        return NextResponse.redirect(new URL(`${locale}/${pathname}`),request.url)
+        return NextResponse.redirect(
+            new URL(`/${locale}/${pathname}`, request.url)
+        );
     }
 
-    return NextResponse.next()
+    return NextResponse.next();
 }
